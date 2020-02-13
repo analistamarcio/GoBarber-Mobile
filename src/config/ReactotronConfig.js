d9c9dfe8d4c578b9a9813bea8280cc06f10e019c
Marcio Ferreira <analistamarcio@hotmail.com>
2020-02-13 13:18:41 -0800

    Setting Redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,10 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron
-    // .configure({ host: 192.168... })  <-  para android
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
     .configure()
     .useReactNative()
     .use(reactotronRedux())
